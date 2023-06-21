39f5f30b8ca207a8a299434b13b8ad4ed839928e
mkg33 <maja.k.gwozdz@gmail.com>
2023-06-21 14:07:38 +0200

    Template

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,19 @@
 using Documenter, VizDataFlow
 
-makedocs(sitename="VizDataFlow")
+makedocs(;
+    pages = [
+        "Home" => "index.md",
+        "Getting started" => "getting_started.md",
+        "Tutorials" => [
+            "Introduction" => "tutorials/index.md",
+        ],
+        "API" => "API/index.md",
+    ],
+    sitename="VizDataFlow.jl",
+)
+
+if get(ENV, "CI", nothing) == "true"
+    deploydocs(;
+        repo="github.com/mkg33/VizDataFlow.jl",
+    )
+end
