58027a2e87fb1df85c128a8432ff2ae0b3ac4444
Maja Gwóźdź <maja.k.gwozdz@gmail.com>
2023-07-18 14:54:51 +0200

    Add information

--- src/read.jl
+++ src/read.jl
@@ -1,14 +1,24 @@
-
-export adios2_init, write_mode, perform_update
+export adios2_init, write_mode, read_mode, perform_update
 
 using ADIOS2
+
+"""
+Sample usage:
+
+adios2_init(joinpath(pwd(),"adios2.xml"), comm) # initialization from XML file, with MPI and an existing comm
+write_mode("temperature", eltype(T)) # define a new variable 'temperature'
+
+"""
 
 let
     global adios2_init, write_mode, perform_update, adios, engine, T_id
 
-    function adios2_init(filename::AbstractString = "",
-                         mpi = true, comm = nothing)
-                         #engine = "SST", var_name = "temperature", var_type = "")
+    """
+    Intialize ADIOS2. Supports several configuration options: MPI, serial,
+    existing XML config file or a new file with specified parameters.
+    """
+    function adios2_init(filename::AbstractString = "", engine = "",
+                         mpi = true, serial = false, comm = nothing)
 
         if isempty(filename)
             if serial == true
@@ -28,86 +38,81 @@ let
             end
         end
 
-        """
-        io = ADIOS2.declare_io(adios, "IO")
+    end
 
-        T_id = define_variable(io, var_name, eltype(var_type))
-
-        bp_path = joinpath(pwd(), "diffusion2D.bp")
-
-        engine = ADIOS2.open(io, bp_path, mode_write)
+    """
+    Initialize io in read mode and the corresponding engine for reading data.
+    """
+    function read_mode(bp_filename = "")
 
         io = ADIOS2.declare_io(adios, "readerIO")
-        """
+        bp_path = joinpath(pwd(), bp_filename)
+        engine = ADIOS2.open(io, bp_path, mode_read)    # Open the file/stream from the .bp file
 
     end
 
-    function read_mode()
-
-    end
-
-    function write_mode(var_name = "", var_type = nothing)
+    """
+    Initialize io in write mode and the corresponding engine for writing data.
+    """
+    function write_mode(var_name = "", var_type = nothing, bp_filename = "")
 
         io = ADIOS2.declare_io(adios, "IO")
-
-        T_id = define_variable(io, var_name, eltype(var_type))
-
-        bp_path = joinpath(pwd(), "diffusion2D.bp")
-
-        engine = ADIOS2.open(io, bp_path, mode_write)
+        T_id = define_variable(io, var_name, eltype(var_type))  # Define a new variable
+        bp_path = joinpath(pwd(), bp_filename)
+        engine = ADIOS2.open(io, bp_path, mode_write)   # Open the file/stream from the .bp file
 
     end
 
+    """
+    Perform the update using specified variables.
+    """
     function perform_update(T_nohalo = nothing)
         begin_step(engine)                                       # Begin ADIOS2 write step
         put!(engine, T_id, T_nohalo)                             # Add T (without halo) to variables for writing
-        end_step(engine)
+        end_step(engine)                                         # End ADIOS2 write step (normally, also includes the actual writing of data)
     end
 
+    """
+    If the ADIOS2 config file is not provided, this function uses the XML
+    template to create the file automatically with parameters provided by the
+    user. Returns the XML path to the new file.
+    """
+    function adios2_config(; engine = "")
 
+        template_path = joinpath(pwd(), "adios2_template.xml")
+        xml_path = joinpath(pwd(), "adios2_config.xml")
 
-function adios2_config(; engine = "")
+        adios2_template = open(template_path, "r")
 
-    template_path = joinpath(pwd(), "adios2_template.xml")
-    xml_path = joinpath(pwd(), "adios2_config.xml")
+        adios2_xml = open(xml_path, "w")
 
-    adios2_template = open(template_path, "r")
+        cp(template_path, xml_path, force = true)
 
-    adios2_xml = open(xml_path, "w")
+        # temp file for replacing modified lines
 
-    cp(template_path, xml_path, force = true)
-
-    # temp file for replacing modified lines
-
-    if cmp(engine, "SST") == 0
-        for line in eachline(xml_path, keep = true)
-            if occursin("ENGINE_TYPE", line)
-                line = "<engine type=\"SST\">"
+        if cmp(engine, "SST") == 0
+            for line in eachline(xml_path, keep = true)
+                if occursin("ENGINE_TYPE", line)
+                    line = "<engine type=\"SST\">"
+                end
+                write(adios2_xml, line)
             end
-            write(adios2_xml, line)
-        end
-    elseif cmp(engine, "BP4") == 0
-        for line in eachline(xml_path, keep = true)
-            if occursin("ENGINE_TYPE", line)
-                line = "<engine type=\"BP4\">"
+        elseif cmp(engine, "BP4") == 0
+            for line in eachline(xml_path, keep = true)
+                if occursin("ENGINE_TYPE", line)
+                    line = "<engine type=\"BP4\">"
+                end
+                write(adios2_xml, line)
             end
-            write(adios2_xml, line)
+
+        else
+            println("Error")
         end
 
-    else
-        println("Error")
+        close(adios2_template)
+        close(adios2_xml)
 
+        return xml_path
     end
 
-    close(adios2_template)
-    close(adios2_xml)
-
-    return xml_path
-
 end
-
-end
-
-#adios2_init()
-
-#adios2_config(engine="SST")
