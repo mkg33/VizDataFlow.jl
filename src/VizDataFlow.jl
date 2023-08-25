49be663ef5ab6d1dc84d9e946bef453f17fe7e08
Maja Gwóźdź <maja.k.gwozdz@gmail.com>
2023-08-25 13:39:30 +0200

    Update VizDataFlow.jl

--- src/VizDataFlow.jl
+++ src/VizDataFlow.jl
@@ -8,7 +8,7 @@ export adios2_init, write_mode, read_mode, perform_update, finalize_adios
 
 let
     global adios2_init, write_mode, read_mode, perform_update, finalize_adios,
-    adios, engine, var, init_state
+    adios, engine, vars, init_state
 
     include("read.jl")
     include("setup.jl")
