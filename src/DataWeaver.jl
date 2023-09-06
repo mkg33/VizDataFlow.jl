4f8d6ce5428515683c2ada1903fda14eb57374eb
Maja Gwóźdź <maja.k.gwozdz@gmail.com>
2023-09-06 21:29:54 +0200

    change name

--- src/DataWeaver.jl
+++ src/DataWeaver.jl
@@ -1,8 +1,8 @@
 """
-Module VizDataFlow
+Module DataWeaver
 """
 
-module VizDataFlow
+module DataWeaver
 
 export adios2_init, write_mode, read_mode, perform_update, finalize_adios,
        inspect_variables, perform_read
@@ -11,11 +11,9 @@ let
     global adios2_init, write_mode, read_mode, perform_update, finalize_adios,
     adios, engine, vars, init_state
 
-    include("read.jl")
-    include("setup.jl")
+    include("rw.jl")
     include("utils.jl")
     include("visualize.jl")
-    include("write.jl")
 
 end
 
--- src/rw.jl
+++ src/rw.jl
@@ -1,17 +1,5 @@
 using ADIOS2
 
-# compare documentation jl vs py
-
-"""
-Sample usage:
-
-adios2_init(filename = "adios2.xml", comm) # initialization from XML file, with
-MPI and an existing comm write_mode("temperature", eltype(T)) # define a new
-variable 'temperature' and provide its type
-
-"""
-    # assign values
-
     """
     Intialize ADIOS2. Supports several configuration options: MPI, serial,
     existing XML config file or a new file with specified parameters.
@@ -21,7 +9,7 @@ variable 'temperature' and provide its type
 
         use_mpi = !isnothing(comm)
 
-        adios = nothing # choose proper type
+        adios = nothing
 
         if isempty(filename)
             if serial
@@ -42,16 +30,15 @@ variable 'temperature' and provide its type
         end
 
         global init_state = true          # mark the initialization step
-        global vars = []           # initialize the variable array (used later for reading and writing)
-
-        # print(@isdefined(adios))
-
+        global vars = []                  # initialize the variable array (used later for reading and writing)
+        global nprocessed = 0            # needed for the reading step
+        global reading_now = false        # flag for the reading loop (to avoid race conditions)
     end
 
     """
     Initialize io in read mode and the corresponding engine for reading data.
     """
-    function read_mode(bp_filename = "") # read_config
+    function read_setup(bp_filename = "")
 
         io = ADIOS2.declare_io(adios, "readerIO")
         bp_path = joinpath(pwd(), bp_filename)
@@ -62,11 +49,7 @@ variable 'temperature' and provide its type
     """
     Initialize io in write mode and the corresponding engine for writing data.
     """
-    function write_mode(variables ...; bp_filename = "") # write_config
-
-        # variable input: "temp", T, "temp1", T1, "temp2", T2 etc.
-
-        # create pairs and read them off
+    function write_setup(variables ...; bp_filename = "")
 
         io = ADIOS2.declare_io(adios, "IO")
 
@@ -75,75 +58,68 @@ variable 'temperature' and provide its type
             push!(vars, var_id)
         end
 
-        bp_path = joinpath(pwd(), bp_filename)          # check formats
+        bp_path = joinpath(pwd(), bp_filename)
         global engine = ADIOS2.open(io, bp_path, mode_write)   # Open the file/stream from the .bp file
 
     end
 
     """
-    Perform the update using specified variables. Change name to write
+    Perform the update using specified variables.
     """
-    function perform_update(update_var = nothing)
+    function write(update_var = nothing ...)
 
-        var = nothing
-
-        for v in vars                                           # Locate the variable to be updated
-            if v == update_var
-                var = v
-                break
-            end
+        for v in vars
+            begin_step(engine)                                       # Begin ADIOS2 write step
+            put!(engine, v, update_var)                              # Add update_var to variables for writing
+            end_step(engine)                                         # End ADIOS2 write step (normally, also includes the actual writing of data)
         end
-
-        begin_step(engine)                                       # Begin ADIOS2 write step
-        put!(engine, vars[1], update_var)                            # Add update_var to variables for writing
-        end_step(engine)                                         # End ADIOS2 write step (normally, also includes the actual writing of data)
 
     end
 
     """
-    Reads the data sequentially. The user may also provide a custom function for handling the data.
+    Encodes the loop condition for the reading step.
     """
 
-    function perform_read(vars...; read_function = nothing, timeout = 100.0, verbose = true) # change name to read
+    function reading(; timeout = 100.0)
 
-        if verbose
-            print("Total number of steps: " * string(steps(engine)))
+        loop_condition = begin_step(engine, step_mode_read, timeout) != step_status_end_of_stream
+
+        if loop_condition
+            global reading_now = true
+            return true
+        else
+            global reading_now = false
+            global nprocessed = 0
+            return false
+    end
+
+    """
+    Reads the data sequentially. This function is particularly useful when called from a loop.
+    """
+
+    function read(var; verbose = true)
+
+        var_id = inquire_variable(io, var)
+
+        if nprocessed == 0
+            nxy_global = shape(var_id)                                               # Extract meta data
+            nxy        = count(var_id)                                               # ...
+            var_type   = type(var_id)                                                # ...
+            global V   = zeros(var_type, nxy)                                        # Preallocate memory for the variable using the meta data
         end
 
-        # pairs
-        # allocate array
+        get(engine, var_id, V)
 
-        for var in vars
+        if reading                                                                  # end the step automatically if loop detected
+            end_step(engine)
+            global nprocessed += 1
+        end
 
-            nprocessed = 0
-
-            while begin_step(engine, step_mode_read, timeout) != step_status_end_of_stream
-
-                var_id = inquire_variable(io, var)
-
-                if nprocessed == 0
-                    nxy_global = shape(var_id)                                               # Extract meta data
-                    nxy        = count(var_id)                                               # ...
-                    var_type   = type(var_id)                                                # ...
-                    global V   = zeros(var_type, nxy)                                        # Preallocate memory for the variable using the meta data
-                end
-
-                if !isnothing(read_function) # the user may handle the data with a custom function
-                    read_function
-                end
-
-                get(engine, var_id, V)
-                end_step(engine)
-
-                if verbose
-                    print("Variable: " * string(var))
-                    print("Step: " * string(nprocessed))
-                    print("Variable steps: " * string(steps(var_iq)))
-                    print("Current step: " * string(current_step(engine)))
-                end
-
-                nprocessed += 1
-            end
+        if verbose
+            print("Variable: " * string(var))
+            print("Step: " * string(nprocessed))
+            print("Variable steps: " * string(steps(var_iq)))
+            print("Current step: " * string(current_step(engine)))
         end
 
     end
@@ -179,7 +155,7 @@ variable 'temperature' and provide its type
     template to create the file automatically with parameters provided by the
     user. Returns the XML path to the new file.
     """
-    function adios2_config(engine_type = "") # write whole as string
+    function adios2_config(engine_type = "")
 
         template_path = joinpath(pwd(), "adios2_template.xml")
         xml_path = joinpath(pwd(), "adios2_config.xml") # save location
